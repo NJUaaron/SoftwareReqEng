2dcddc0901622fa1c0d713371889e56b6122ac13
ME-Msc <45875501+ME-Msc@users.noreply.github.com>
2019-10-17 19:54:08 +0800

    Update Separate.py

--- Exp1/Separate.py
+++ Exp1/Separate.py
@@ -5,26 +5,27 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 
-df = pd.read_csv('test.csv', header = None, error_bad_lines = False, encoding='gb18030')
+df = pd.read_csv('raw.csv', header = None, error_bad_lines = False, encoding='gb18030')
 # df.columns = ['num', 'title', 'text']
-print(df)
-print()
+# print(df)
+# print()
 for i in range(len(df)):
     words = []
     text = df.iat[i,1] + ',' + df.iat[i,2]
     doc = nlp(text)
     for nounc in doc.noun_chunks:
         words.append(nounc.text)
-    print(words)
-    print()
+    # print(words)
+    # print()
     string = ""
     for j in range(len(words)):
         string = string + "".join(words[j]) 
         string = string + ","
     string = string[:-1]
-    print(string)
-    print()
+    # print(string)
+    # print()
     df.iat[i,2] = string
 
-print(df)
-df.to_csv('result.csv', header = 0, index = 0)
+# print(df)
+print("Separate words successfully!")
+df.to_csv('word.csv', header = 0, index = 0)
