fdea4484a139fc05391650b130dc31047512746e
ME-Msc <45875501+ME-Msc@users.noreply.github.com>
2019-10-18 22:30:04 +0800

    Update Separate.py

--- Exp1/Separate.py
+++ Exp1/Separate.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import spacy
+import re
 
+patn = re.compile(r'^[Tt]he\b ?|^[Aa]\b ?|^[Aa]n\b ?|^I\b ?|^[Mm]e\b ?|^[Yy]ou\b ?|^[Hh]e\b ?|^[Ss]he\b ?|^[Ii]t\b ?|^[Ww]e\b ?|^[Tt]hey\b ?|^[Mm]y ?|^[Yy]our ?|^[Hh]is ?|^[Hh]er ?|^[Tt]heir\b ?|^[Ww]hat ?|^[Ww]ho ?|^[Ww]here ?|^[Ss]ome ?|^[Aa]ll\b ?|^[Aa]ny\b ?|^[Oo]ne ?|^[Tt]his\b ?|^[Tt]hat\b|^[Ss]uch a ?|^[Ss]o\b ?')
 nlp = spacy.load("en_core_web_sm")
 
 df = pd.read_csv('raw.csv', error_bad_lines = False, encoding='utf8')
@@ -13,12 +15,16 @@ for i in range(len(df)):
     doc = nlp(content)
     for nounc in doc.noun_chunks:
         words.append(nounc.text)
-    print(words)
+    # print(words)
     
     for j in range(len(words)):
-        fp.write(words[j])
-        if j < len(words)-1:
-            fp.write(',')
+        string = ""
+        string = words[j]
+        str = re.sub(patn, '', string)
+        if len(str)>1:
+            fp.write(str)
+            if j < len(words)-1:
+                fp.write(',')
     fp.write('\n')
 
 fp.close()
