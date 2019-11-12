9024fbbea16a896c39f82f7ad882478577852a04
ME-Msc <45875501+ME-Msc@users.noreply.github.com>
2019-11-12 22:17:22 +0800

    Update Seperate.py

---
+++ Exp2/Code/Seperate.py
@@ -0,0 +1,50 @@
+import os
+
+import nltk
+import pandas as pd
+from nltk.corpus import stopwords
+
+pwd = os.getcwd()
+father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + ".")
+read_path = father_path+'\\Data\\Raw'
+save_path = father_path+'\\Data\\Word'
+
+stopWords = set(stopwords.words('english'))
+# print(stopWords)
+
+files = os.listdir(read_path)
+files_csv = list(filter(lambda x: x[-4:] == '.csv', files))
+for file in files_csv:
+    # print(read_path+'\\'+file)
+    df = pd.read_csv(read_path+'\\'+file, header=None, names=['No','Title','Content','Level'])
+    fp = open(save_path+'\\'+file, 'w+', encoding='utf8')
+    for i in range(len(df)):
+        fp.write(df.iat[i, 0].astype(str) + ',')
+        text = df.iat[i, 1] + ' '+ df.iat[i, 2]
+        disease_List = nltk.word_tokenize(text)
+        filtered = []
+        for w in disease_List:
+            if w not in stopwords.words('english'):
+                filtered.append(w)
+        Rfiltered = nltk.pos_tag(filtered)
+        for j in range(len(Rfiltered)):
+            fp.write(Rfiltered[j][0]+' ')
+        fp.write(',' + df.iat[i, 3].astype(str) + '\n')
+    fp.close()
+    print('Seperate ' + read_path+file + ' DONE')
+
+"""
+    for i in range(len(df)):
+        text = df.iat[i, 1] + ' '+ df.iat[i, 2]
+        words = word_tokenize(text)
+        fp.write(df.iat[i, 0].astype(str) + ',')
+        # wordsFiltered = []
+        for w in words:
+            if w not in stopWords:
+                fp.write(w + ' ')
+                # wordsFiltered.append(w)
+        # print(wordsFiltered)
+        fp.write(',' + df.iat[i, 3].astype(str) + '\n')
+    fp.close()
+    print('@@@@@@@@@@@' + read_path+file + ' DONE')
+"""
