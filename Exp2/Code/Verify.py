18cb5d0f4702730f2e6cd1e35c8bcf36e1009352
ME-Msc <45875501+ME-Msc@users.noreply.github.com>
2019-11-13 20:36:08 +0800

    Update Verify.py

--- Exp2/Code/Verify.py
+++ Exp2/Code/Verify.py
@@ -12,11 +12,16 @@ for file in file_csv:
     df_ans = pd.read_csv(ans_path+'\\'+file, header=None, names=['Level','Prediction'])
     cnt1 = 0
     cnt2 = 0
+    cnt3 = 0
     for i in range(len(df_ans)):
-        if df_ans.iat[i, 0].astype(int) == df_ans.iat[i, 1].astype(int):
+        if abs(df_ans.iat[i, 0].astype(float) - df_ans.iat[i, 1].astype(float)) <= 0.5:
             cnt1 = cnt1 + 1
-        if abs(df_ans.iat[i, 0].astype(int) - df_ans.iat[i, 1].astype(int)) <= 1:
+        if abs(df_ans.iat[i, 0].astype(float) - df_ans.iat[i, 1].astype(float)) <= 1:
             cnt2 = cnt2 + 1
+        if abs(df_ans.iat[i, 0].astype(float) - df_ans.iat[i, 1].astype(float)) <= 1.5:
+            cnt3 = cnt3 + 1
     print('Verify ' + ans_path + '\\' + file + ' AccuracyRate1 = ' + str(cnt1 / len(df_ans)))
     print('Verify ' + ans_path + '\\' + file + ' AccuracyRate2 = ' + str(cnt2 / len(df_ans)))
+    print('Verify ' + ans_path + '\\' + file + ' AccuracyRate3 = ' + str(cnt3 / len(df_ans)))
+    print('--------------------------------------------------------------------------------')
 print("Verify successfully!")
