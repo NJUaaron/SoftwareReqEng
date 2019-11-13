a82b1a17808b1326ae4660b1dfe5f1a1f950fa9c
ME-Msc <45875501+ME-Msc@users.noreply.github.com>
2019-11-13 20:28:59 +0800

    Update Verify.py

--- Exp2/Code/Verify.py
+++ Exp2/Code/Verify.py
@@ -1,27 +1,22 @@
 import os
-
-import nltk
 import pandas as pd
 
 pwd = os.getcwd()
 father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + ".")
-raw_path = father_path+'\\Data\\Raw'
 ans_path = father_path+'\\Data\\Prediction'
-raw_files = os.listdir(raw_path)
 ans_files = os.listdir(ans_path)
-file_csv = list(filter(lambda x: x[-4:] == '.csv', raw_files))
+file_csv = list(filter(lambda x: x[-4:] == '.csv', ans_files))
 
 
 for file in file_csv:
-    df_raw = pd.read_csv(raw_path+'\\'+file, header=None, names=['No','Title','Content','Level'])
-    df_ans = pd.read_csv(ans_path+'\\'+file, header=None, names=['No','Ans'])
+    df_ans = pd.read_csv(ans_path+'\\'+file, header=None, names=['Level','Prediction'])
     cnt1 = 0
     cnt2 = 0
-    for i in range(len(df_raw)):
-        if df_raw.iat[i, 3].astype(int) == df_ans.iat[i, 1].astype(int):
+    for i in range(len(df_ans)):
+        if df_ans.iat[i, 0].astype(int) == df_ans.iat[i, 1].astype(int):
             cnt1 = cnt1 + 1
-        if abs(df_raw.iat[i, 3].astype(int) - df_ans.iat[i, 1].astype(int)) <= 1:
+        if abs(df_ans.iat[i, 0].astype(int) - df_ans.iat[i, 1].astype(int)) <= 1:
             cnt2 = cnt2 + 1
-    print('Verify ' + ans_path + '\\' + file + ' AccuracyRate1 = ' + str(cnt1 / len(df_raw)))
-    print('Verify ' + ans_path + '\\' + file + ' AccuracyRate2 = ' + str(cnt2 / len(df_raw)))
+    print('Verify ' + ans_path + '\\' + file + ' AccuracyRate1 = ' + str(cnt1 / len(df_ans)))
+    print('Verify ' + ans_path + '\\' + file + ' AccuracyRate2 = ' + str(cnt2 / len(df_ans)))
 print("Verify successfully!")
